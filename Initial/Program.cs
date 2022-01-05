316c89adb0047a4fdd93e0b846c4ee06324ace5d
sananabbasov <sanan_abbasov@list.ru>
2022-01-05 15:00:04 +0400

    05.01.2022 15:00

---
+++ Initial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Initial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety
+
+
+            // data type 
+            string ad = "1";
+            string soyad = "Muradov";
+            int yas = 999999999;
+            decimal boy =  1.5M;
+            bool telebe = false;
+            long yas2 = 99999999999999;
+
+
+
+            // float, bool, double  
+
+
+
+            Console.WriteLine("Adinizi daxil edin:");
+        }
+    }
+}
